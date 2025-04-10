d43d63fb8784a4f8dce52516656b9a92dc18b9b6
tugbaguneyy <200202089@kocaeli.edu.tr>
2025-04-10 22:13:33 +0300

    Initial commit - basic counter app without ViewModel

---
+++ app/src/main/java/com/example/viewmodelexample/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.viewmodelexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.viewmodelexample.ui.theme.ViewModelExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ViewModelExampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyCounterApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyCounterApp() {
+    var count by remember{ mutableStateOf(0) }
+    fun increment(){
+        count += 1
+    }
+    fun decrement(){
+        count--
+    }
+    //user interface
+    Column( modifier=Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally ) {
+        Text("Counter: $count",
+            fontSize   = 24.sp,
+            fontWeight = FontWeight.Bold)
+        Button(onClick = { increment() }) {
+            Text(text = "Increment")
+        }
+        Button(onClick = { decrement() }) {
+            Text(text = "Decrement")
+        }
+    }
+
+
+}
